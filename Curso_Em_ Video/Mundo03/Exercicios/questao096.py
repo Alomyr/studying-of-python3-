9f72d21494a79960a9bfec7c7f45b6c5fd28366c
Alomyr <matheuscastro11213@gmail.com>
2026-01-14 16:29:38 -0300

    funcao area

---
+++ Curso_Em_ Video/Mundo03/Exercicios/questao096.py
@@ -0,0 +1,9 @@
+def area(largura, comprimento):
+    area = largura * comprimento
+    return area
+
+
+largura = float(input("Digite o valor da largura: "))
+comprimento = float(input("Digite o valor do comprimento: "))
+
+print(area(largura, comprimento))
