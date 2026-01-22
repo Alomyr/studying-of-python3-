56ceb064e375f7a17b039443424fae13894746ca
Alomyr <matheuscastro11213@gmail.com>
2026-01-22 10:35:40 -0300

    sobe o inicio da aula 22

---
+++ Curso_Em_ Video/Mundo03/Modulos/main.py
@@ -0,0 +1,9 @@
+import util
+
+numero = int(input("Digite um numero: "))
+
+numero = util.dobro(numero)
+nome=str(input("Digite seu nome: "))
+nome = util.nome(nome)
+print(nome)
+print(numero)
---
+++ Curso_Em_ Video/Mundo03/Modulos/util.py
@@ -0,0 +1,5 @@
+def dobro(n):
+    return  n*2
+
+def nome(nome = str()):
+    return nome
