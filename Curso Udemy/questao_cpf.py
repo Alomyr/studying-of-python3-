61053fb543bd766a8e5fb9506ca42ff911be02b5
alomyr <matheuscastro11213@gmail.com>
2026-04-07 15:26:28 -0300

    sobe as ultimas questoes do curso

--- Curso Udemy/questao_cpf.py
+++ Curso Udemy/questao_cpf.py
@@ -1,19 +1,22 @@
 import copy
+import random
 
+r = random
+cpf_gerado = [random.randint(0, 9) for _ in range(8)]
+print(cpf_gerado)
+# cpf = int(input("digite os 9 (nove) primeiros digitos do seu CPF:"))
+# cpf_gerado = list(map(int, str(cpf)))
 
-cpf = int(input("digite os 9 (nove) primeiros digitos do seu CPF:"))
-list_cpf = list(map(int, str(cpf)))
-
-while len(list_cpf) < 11:
-    numeros = len(list_cpf) + 1
+while len(cpf_gerado) < 11:
+    numeros = len(cpf_gerado) + 1
     soma = 0
-    for i in list_cpf:
+    for i in cpf_gerado:
         soma += numeros * i
         numeros -= 1
 
     digito = (soma * 10) % 11
     if digito > 9:
-            digito = 0
-    list_cpf.append(digito)
+        digito = 0
+    cpf_gerado.append(digito)
 
-print(list_cpf)
+print(cpf_gerado)
