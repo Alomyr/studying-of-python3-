efc13510f8dd53b927af7c41485b534caf9ed53c
Alomyr <matheuscastro11213@gmail.com>
2026-04-27 19:37:01 -0300

    questão duplicata

---
+++ Curso Udemy/questao_duplicada.py
@@ -0,0 +1,26 @@
+def duplicado(lista):
+
+    primeiro_duplicado = []
+    
+    for i in range(len(lista)):
+        underset = set()
+        is_duplique = False
+        
+        for j in range(len(lista[i])):
+
+
+            if lista[i][j] in underset:
+                primeiro_duplicado.append(lista[i][j])
+                is_duplique = True
+                break
+              
+            underset.add(lista[i][j])
+            
+        if not is_duplique:
+            primeiro_duplicado.append(-1)
+            
+    return primeiro_duplicado
+
+
+lista = [[1, 1, 2], [3, 3, 2], [1, 2, 3]]
+print(duplicado(lista))
